3f397187c385a8870850833cb98a5efa09cb1397
jessehancock <jessehancock1@gmail.com>
2016-07-25 10:51:30 -0600

    have the modal working

--- public/bundle.js
+++ public/bundle.js
@@ -2,80 +2,78 @@
 // INITILIZE APP
 // ============================================================
 angular.module("app", []);
-
 // INITILIZE CONTROLLER
 // ============================================================
-angular.module("app").controller("mainCtrl", ["$scope", "mainServ", function($scope, mainServ) {
-    $scope.apiKey = '8ff7b56';
+angular.module("app").controller("mainCtrl", ["$scope", "mainServ", function ($scope, mainServ) {
+  $scope.apiKey = '8ff7b56';
 
-    $scope.moviefunc = function(title){
-      mainServ.getMovie(title).then(function(response){
-        $scope.displayMovies = response;
+  $scope.moviefunc = function (title) {
+    //set background gif
+    mainServ.getMovie(title).then(function (response) {
+      //set background off
+      $scope.displayMovies = response;
+    });
+  };
 
-      });
-    };
-
-    $scope.secondfunc = function(id){
+  $scope.secondfunc = function (id) {
     console.log(id);
-      mainServ.getDisc(id).then(function(response){
-        console.log(response)
-        $scope.displayMovie = response;
-        });
-    };
+    mainServ.getDisc(id).then(function (response) {
+      console.log(response);
+      $scope.displayMovie = response;
+    });
+  };
 
-
-
+  $scope.hello = function (input) {
+    console.log(input);
+  };
 }]);
-
 // INITILIZE SERVICE
 // ============================================================
-angular.module("app").service("mainServ", ["$http", "$q", function($http, $q) {
-    // CRUD FUNCTIONS
-    // ============================================================
-    var searchURL = 'http://www.omdbapi.com/?s=';
-    var searchID = 'http://www.omdbapi.com/?i=';
+angular.module("app").service("mainServ", ["$http", "$q", function ($http, $q) {
+  // CRUD FUNCTIONS
+  // ============================================================
+  var searchURL = 'http://www.omdbapi.com/?s=';
+  var searchID = 'http://www.omdbapi.com/?i=';
 
-
-    this.getMovie = function(title) {
-      var counter = 1;
-      var returnData = [];
-      var defer = $q.defer();
-      var filterData = function() {
-        $http({
-          method: 'GET',
-          url: searchURL + title + '&type=movie&page=' + counter
-        }).then(function(response) {
-          if (response.data.Response === "False") {
-            console.log(counter, returnData);
-            defer.resolve(returnData);
+  this.getMovie = function (title) {
+    var counter = 1;
+    var returnData = [];
+    var defer = $q.defer();
+    var filterData = function () {
+      $http({
+        method: 'GET',
+        url: searchURL + title + '&type=movie&page=' + counter
+      }).then(function (response) {
+        if (response.data.Response === "False") {
+          console.log(counter, returnData);
+          defer.resolve(returnData);
+        }
+        if (response.data.Search.length) {
+          console.log(response.data);
+          var resultsArr = response.data.Search;
+          for (var i = resultsArr.length - 1; i > -1; i--) {
+            if (resultsArr[i].Poster === 'N/A') resultsArr.splice(i, 1);
           }
-          if(response.data.Search.length) {
-            console.log(response.data);
-            var resultsArr = response.data.Search;
-            for (var i = resultsArr.length - 1; i > -1; i--) {
-              if (resultsArr[i].Poster === 'N/A') resultsArr.splice(i, 1);
-            }
-            returnData.push(...resultsArr);
-          }
-          counter++;
-          filterData();
-        });
-      };
+          returnData.push(...resultsArr);
+        }
+        counter++;
+        filterData();
+      });
+    };
     filterData();
     return defer.promise;
-};
+  };
 
-    this.getDisc = function(id) {
-        return $http({
-            method: 'GET',
-            url: searchID + id
-        }).then(function(response) {
-            console.log(response);
-            return response.data;
-        });
-    };
+  this.getDisc = function (id) {
+    return $http({
+      method: 'GET',
+      url: searchID + id
+    }).then(function (response) {
+      console.log(response);
+      return response.data;
+    });
+  };
 
-    // OTHER FUNCTIONS
-    // ============================================================
-
+  // OTHER FUNCTIONS
+  // ============================================================
 }]);
--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -4,7 +4,9 @@ angular.module("app").controller("mainCtrl", function($scope, mainServ) {
     $scope.apiKey = '8ff7b56';
 
     $scope.moviefunc = function(title){
+      //set background gif
       mainServ.getMovie(title).then(function(response){
+        //set background off
         $scope.displayMovies = response;
 
       });
@@ -13,9 +15,13 @@ angular.module("app").controller("mainCtrl", function($scope, mainServ) {
     $scope.secondfunc = function(id){
     console.log(id);
       mainServ.getDisc(id).then(function(response){
-        console.log(response)
+        console.log(response);
         $scope.displayMovie = response;
         });
+    };
+
+    $scope.hello = function(input){
+      console.log(input);
     };
 
 
